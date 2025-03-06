2f774451ea2bf64aa877ab31e68739e42ccf7ff1
JPJSea <emailJessP@gmail.com>
2025-03-06 22:28:05 +0000

    Refactor utils.py

--- robot_simulation/utils.py
+++ robot_simulation/utils.py
@@ -1,24 +1,31 @@
 import pygame
+from pygame import Surface
+
 from robot_simulation.config import Config
 from robot_simulation.robot import Robot
+from robot_simulation.grid import Grid
 
 def handle_events(robot: Robot) -> bool:
     """Handle quit event and movement keys."""
+
+    key_to_direction = {
+        pygame.K_UP: "up",
+        pygame.K_DOWN: "down",
+        pygame.K_LEFT: "left",
+        pygame.K_RIGHT: "right",
+    }
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                robot.move("up")
-            elif event.key == pygame.K_DOWN:
-                robot.move("down")
-            elif event.key == pygame.K_LEFT:
-                robot.move("left")
-            elif event.key == pygame.K_RIGHT:
-                robot.move("right")
+            direction = key_to_direction.get(event.key)
+            if direction:
+                robot.move(direction)
+
     return True
 
-def render_screen(screen, grid, robot):
+def render_screen(screen: Surface, grid: Grid, robot: Robot) -> None:
     """Render the grid and the robot on the screen."""
     screen.fill(Config.WHITE)
     grid.draw(screen)
